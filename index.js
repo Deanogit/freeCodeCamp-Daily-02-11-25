8af7b0d6071469336e5f03cab31e101d09aaa6e9
Dean Howe <hideanhowe@gmail.com>
2025-12-02 14:09:43 +0000

    still working

--- index.js
+++ index.js
@@ -74,15 +74,25 @@
 function infected(days) {
   console.log('This is what we start with: ', days);
   let counter = days + 1;
-  console.log('This is what we have when we add 1: ', counter);
-  counter ** 2;
-  console.log('This is what we have when we power 2: ', counter);
-  // check if days mod 3
-  if (days % 3 == 0) {
-    let discount = (counter / 100) * 20;
-    console.log(discount);
+  for (let i = 1; i <= days; i++) {
+    console.log('This is what we have when we add 1: ', counter);
+    counter = counter + counter;
+    console.log(
+      'This is what we have when we add counter to counter: ',
+      counter
+    );
+    // check if days mod 3
+    if (days % 3 == 0) {
+      let discount = Math.ceil((counter / 100) * 20);
+      console.log(discount);
+      counter = counter - discount;
+    }
+    return counter;
   }
-  return days;
+  return counter;
 }
 
+infected(0);
+infected(1);
+infected(2);
 infected(3);
