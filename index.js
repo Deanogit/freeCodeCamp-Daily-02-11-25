0e3ebd77d7522d48054b2238d8fab11908c47f22
Dean Howe <hideanhowe@gmail.com>
2025-12-02 13:51:36 +0000

    testing

--- index.js
+++ index.js
@@ -14,21 +14,42 @@
 // Day 3: 8 total computers are infected. Then, apply the patch: 8 infected * 20% = 1.6 patched. Round 1.6 up to 2. 8 computers infected - 2 patched = 6 total computers infected after day 3.
 // Return the number of total infected computers after the given amount of days have passed.
 
-function infected(days) {
-  let counter = 1;
-  for (let i = 1; i <= days; i++) {
-    counter = counter ** 2;
-    if (days % 3 == 0) {
-      console.log('3!');
-      console.log(counter);
-    }
-    console.log(counter);
-    counter ** 2;
-    return counter;
-  }
-}
+//function infected(days) {
+// counter
+//  let counter = 1;
+// if (days == 0) {
+//  return counter
+// use a loop
+// }
 
-infected(3);
+// else if(days >= 1 && days % 3 != 0){
+// console.log(days)
+//   return counter
+// } else {
+//  console.log(days)
+//  console.log(days + days)
+//  console.log((days + days) - (Number.parseFloat(days + days / 100 // * 20).toFixed(2)))
+//   return (days + days) - (Number.parseFloat(days + days / 100 * 20).toFixed(2))
+// }
+// }
+
+// infected(3)
+
+// function infected(days){
+//  let counter = 1;
+//  for (let i = 1; i <= days; i++ ) {
+//      counter = counter ** 2;
+//    if (days % 3 == 0) {
+//      console.log("3!")
+//      console.log(counter)
+//    }
+//  console.log(counter)
+//  counter ** 2
+//    return counter
+//  }
+// }
+
+//infected(3)
 
 // Squaring a number using the exponentiation operator
 // const number = 5;
@@ -36,3 +57,18 @@ infected(3);
 // console.log(squared);
 
 // Output: 25
+
+function infected(days) {
+  if (days == 0) {
+    return 1;
+  } else if (days == 1) {
+    return 2;
+  } else {
+    if (days % 3 == 0) {
+      console.log(days ** 2);
+    }
+    return days ** 2;
+  }
+}
+
+infected(3);
